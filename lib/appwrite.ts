8287c1b307ced964ba54fff964531ed8708f0801
ajithkaranam <ajithkarnam@gmail.com>
2025-01-15 21:49:24 +0530

    Signed-off-by: ajithkaranam <ajithkarnam@gmail.com>

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -12,18 +12,16 @@ import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
-  platform: "com.jsm.restate",
+  platform: "com.refe",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
   galleriesCollectionId:
     process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
   reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
-  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
   propertiesCollectionId:
     process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
   bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
-  jobtypeCollectionId:process.env.EXPO_PUBLIC_APPWRITE_JOBTYPE_COLLECTION_ID
 };
 
 export const client = new Client();
@@ -39,7 +37,7 @@ export const storage = new Storage(client);
 
 export async function login() {
   try {
-    const redirectUri = Linking.createURL("/");
+    const redirectUri = Linking.createURL("/", { scheme: "com.ajithkaranam.refe" });
 
     const response = await account.createOAuth2Token(
       OAuthProvider.Google,
