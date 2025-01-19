25e7e2124511f541203cd9be97440347d420d977
ajith kumar <ajithkarnam@gmail.com>
2025-01-19 13:39:35 +0530

    Signed-off-by: ajith kumar <ajithkarnam@gmail.com>

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -11,7 +11,7 @@ const OnBoardingPage = () => {
   const { loading, isLogged } = useGlobalContext();
   if (!loading && isLogged) return <Redirect href="/" />;
   const handleOnboarding = async () => {
-    router.push('/sign-in');
+    router.push('/otp-verify');
   };
   return (
     <SafeAreaView className="bg-white h-full">
@@ -22,7 +22,7 @@ const OnBoardingPage = () => {
       >
         <Image
           source={images.onboarding}
-          className="w-full h-4/6"
+          className="w-full h-4/6 rounded-[100px]"
           resizeMode="contain"
         />
         <View className="px-10">
@@ -30,21 +30,18 @@ const OnBoardingPage = () => {
             Find Your Nearest{'\n'}
             <Text className="text-primary-300">Talent with Refe</Text>
           </Text>
-          <View>
-            <TouchableOpacity
-              onPress={handleOnboarding}
-              className=" bg-primary-300 shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5 justify-center items-center"
-            >
-              <View className="flex flex-row items-center justify-center ">
-                <Text className="text-lg font-rubik-medium text-white ml-2">
-                  Continue with Mobile
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <Text className=" font-rubik text-center mt-2 text-gray-400">
-            Your Talent Need To Hire
-          </Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={handleOnboarding}
+            className=" bg-primary-300 shadow-md shadow-zinc-300 rounded-[12px] mx-8 py-5 mt-10 justify-center items-center"
+          >
+            <View className="flex flex-row items-center justify-center ">
+              <Text className="text-lg font-rubik-medium text-white ml-2">
+                Continue with Mobile
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </ScrollView>
       <StatusBar style="dark" />
